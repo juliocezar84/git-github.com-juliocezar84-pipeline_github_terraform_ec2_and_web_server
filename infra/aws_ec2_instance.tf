3232b4f304087ccf0bd97e4c2541851849c2de41
Julio Vicente <juliocezar84@gmail.com>
2024-09-28 11:23:37 -0300

    commit

--- infra/aws_ec2_instance.tf
+++ infra/aws_ec2_instance.tf
@@ -7,7 +7,7 @@ module "aws_ec2_instance" {
   instance_type          = var.instanceType
   key_name               = var.keyName
   monitoring             = true
-  vpc_security_group_ids = [module.aws_security_group.security_group_id]
+  vpc_security_group_ids = [module.aws_security_group.security_group_id, aws_security_group.aws_ssh_access.id]
   user_data = file("files/install_http.sh")
 
   depends_on = [module.aws_security_group, aws_security_group.aws_ssh_access]
